68bb7d0f8be76060ad1704da3b5a184cc32db5ce
bamshak9 <b.fwangyil@alustudent.com>
2025-07-16 15:41:48 +0100

    added shortcut class task

---
+++ floor.py
@@ -0,0 +1,28 @@
+first_number = 5
+first_number += 3
+print(first_number)
+
+first_number = 5
+first_number -= 3
+print(first_number)
+
+first_number = 5
+first_number *= 3
+print(first_number)
+
+first_number = 5
+first_number /= 3
+print(first_number)
+
+first_number = 5
+first_number **= 3
+print(first_number)
+
+first_number = 5
+first_number %= 3
+print(first_number)
+
+first_number = 5
+first_number //= 3
+print(first_number)
+
