1c09ac113efd0650a77e2fa24728ea4279cec113
bamshak9 <b.fwangyil@alustudent.com>
2025-07-16 11:12:12 +0100

    first commit

---
+++ boolean.py
@@ -0,0 +1,10 @@
+isMarried= True
+isMarried= False
+print(isMarried)
+#Datetype
+#print(type(isMarried))
+first_name= "Tom"
+my_favorite_number = 0
+print(bool(first_name))
+#print(bool(my_favorite_number))
+
