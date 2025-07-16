47d4525154726998d203b6dfeb8c064e1af68053
bamshak9 <b.fwangyil@alustudent.com>
2025-07-16 11:37:19 +0100

    added int2

--- boolean.py
+++ boolean.py
@@ -1,10 +1,25 @@
 isMarried= True
 isMarried= False
-print(isMarried)
+#print(isMarried)
 #Datetype
 #print(type(isMarried))
-first_name= "Tom"
-my_favorite_number = 0
-print(bool(first_name))
+#first_name= "Tom"
+#my_favorite_number = 0
+#print(bool(first_name))
 #print(bool(my_favorite_number))
-
+my_name="Bamshak"
+my_main= 'Skirk'
+her_name=''
+her_main=""
+print(bool(my_name))
+print(bool(my_main))
+print(bool(her_name))
+print(bool(her_main))
+scared_number= 6
+scary_number= 7
+no_number= 0
+no_numbre= 0.00
+print(bool(scared_number))
+print(bool(scary_number))
+print(bool(no_number))
+print(bool(no_numbre))
